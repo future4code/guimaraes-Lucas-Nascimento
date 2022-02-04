10d5c861c3b915aeef0643e4c1d565981e052024
unknown <lucastheborges@gmail.com>
2022-02-04 20:49:12 -0300

    Commit inicial

---
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const BodyWhats = styled.div`
+  
+
+`
+class App extends React.Component {
+
+  state = {
+  }
+
+
+
+  render() {
+    return (
+      <div>
+       
+      </div>
+      
+
+    );
+  }
+}
+
+export default App;

a702bd550a6f42f41369fb8821ff9ddfab0af642
unknown <lucastheborges@gmail.com>
2022-02-06 15:19:21 -0300

    finalização do projeto whatslab

--- modulo3/projeto-whatslab/whatslab/src/App.js
+++ modulo3/projeto-whatslab/whatslab/src/App.js
@@ -1,25 +1,125 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components'
+import Msg from './components/Msg/CampoMsg';
 
 
 const BodyWhats = styled.div`
-  
-
+  background-color: #E0E0E0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: flex-end;
+  width: 600px;
+  height: 95vh;
+  box-shadow: 2px 2px 12px darkgray;
+  background-color: lightgray;
+  border: 1px solid #555;
+  border-radius: 20px;
+  padding: 20px;
+`
+const PostMsg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+`
+const InputForm = styled.input`
+  border: 0;
+  border-radius: 20px;
+  padding: 8px;
+  outline: 0;
+  box-shadow: 2px 2px 12px darkgray;
+  margin-right: 10px;
+`
+const BtnForm = styled.button`
+  background-color: lightgreen;
+  border: 0;
+  padding: 8px;
+  border-radius: 20px;
+  font-weight: bold;
+  box-shadow: 2px 2px 12px darkgray;
+`
+
+
 class App extends React.Component {
 
   state = {
+    msg: [
+
+    ],
+
+    valorInputNome: '',
+    valorInputMsg: '',
+
+  }
+
+  adicionaMsg = () => {
+    const novaMsg = {
+      nome: this.state.valorInputNome,
+
+      msgUsuario: this.state.valorInputMsg,
+
+
+    }
+
+    const novaMsgUsuario = [...this.state.msg, novaMsg]
+
+    this.setState({ msg: novaMsgUsuario })
+
+    this.setState({ valorInputMsg: '' }) // Função para limpar o campo do input assim que clicar no botao de enviar
+
+  }
+
+  onChangeInputNome = (event) => {
+    this.setState({ valorInputNome: event.target.value })
+  }
+
+  onChangeInputMsg = (event) => {
+    this.setState({ valorInputMsg: event.target.value })
   }
 
 
-
   render() {
-    return (
-      <div>
-       
-      </div>
-      
+    const listaDeMsg = this.state.msg.map((msg) => {
+      return (
+        
+        <Msg nomeUsuario={msg.nome} msgUsuario={msg.msgUsuario} /> 
 
+      )
+    })
+
+
+    return (
+      <BodyWhats>
+
+
+        <MainContainer>{listaDeMsg}</MainContainer>
+
+        <PostMsg>
+          <InputForm
+            value={this.state.valorInputNome}
+            onChange={this.onChangeInputNome}
+            placeholder={'Usuario'}
+          />
+          <InputForm
+            value={this.state.valorInputMsg}
+            onChange={this.onChangeInputMsg}
+            placeholder={'Digite uma mensagem...'}
+          />
+
+          <BtnForm onClick={this.adicionaMsg}>Enviar</BtnForm>
+
+        </PostMsg>
+
+
+
+      </BodyWhats>
     );
   }
 }
---
+++ modulo3/projeto-whatslab/whatslab/src/components/SecaoComentario/SecaoComentario.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+
+const CommentContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: 5px;
+`
+
+const InputComentario = styled.input`
+    width: 100%;
+    margin-right: 5px;
+	border: 0;
+	border-radius: 20px;
+	outline: 0;
+	padding: 5px;
+`
+const BtnComentario = styled.button `
+	border: 0;
+	border-radius: 20px;
+	background-color: lightgreen;
+	padding: 5px;
+	font-weight: bold;
+`
+
+
+export class SecaoComentario extends Component {
+	state = {
+		inputComent: ''
+	}
+
+	onChangeComentario =  (e) => {
+		console.log(e.target.value);
+		this.setState({inputComent: e.target.value})
+	}
+
+	render() {
+		return <CommentContainer>
+			<InputComentario
+				placeholder={'Comentário'}
+				value={this.state.inputComent}
+				onChange={this.onChangeComentario}
+
+			/>
+			<BtnComentario onClick={this.props.aoEnviar}>Enviar</BtnComentario>
+		</CommentContainer>
+	}
+}
